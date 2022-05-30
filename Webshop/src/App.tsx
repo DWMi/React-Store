b39915b4e2606026b02329636a56777a9319a9cb
antoniowav <85946713+antoniowav@users.noreply.github.com>
2022-05-30 15:35:43 +0200

    imported project and installed MUI + React

---
+++ Webshop/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1>Webshop</h1>
+    </div>
+  )
+}
+
+export default App
