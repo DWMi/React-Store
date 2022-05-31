f4dadb80ffebabebba37448105bd821d82b6cb20
David w <cf.davidwong@gmail.com>
2022-05-31 13:53:33 +0200

    added some stuff in to footer component
    #23

--- Webshop/src/data/productList.tsx
+++ Webshop/src/data/productList.tsx
@@ -1,27 +1,27 @@
-import dunHill1 from './src/assets/Dunhill1.webp'
-import dunHill2 from './src/assets/Dunhill2.webp'
-import tomFord1 from './src/assets/Tomford1.webp'
-import tomFord2 from './src/assets/Tomford2.webp'
-import dunHillClear1 from './src/assets/DunhillClear1.webp'
-import dunHillClear2 from './src/assets/DunhillClear2.webp'
-import AMCQ1 from './src/assets/AlexanderMcQueen1.webp'
-import AMCQ2 from './src/assets/AlexanderMcQueen2.webp'
-import montBlanc1 from './src/assets/MontBlanc1.webp'
-import montBlanc2 from './src/assets/MontBlanc2.webp'
-import gucci1 from './src/assets/Gucci1.webp'
-import gucci2 from './src/assets/Gucci2.webp'
-import prada1 from './src/assets/prada1.webp'
-import prada2 from './src/assets/prada2.webp'
-import DB1 from './src/assets/Dolcegabbana1.webp'
-import DB2 from './src/assets/Dolcegabbana2.webp'
-import tods1 from './src/assets/Tods1.webp'
-import tods2 from './src/assets/Tods2.webp'
-import versace1 from './src/assets/Versace1.webp'
-import versace2 from './src/assets/Versace2.webp'
+import dunHillOne from './src/assets/Dunhill1.webp'
+import dunHillTwo from './src/assets/Dunhill2.webp'
+import tomFordOne from './src/assets/Tomford1.webp'
+import tomFordTwo from './src/assets/Tomford2.webp'
+import dunHillClearOne from './src/assets/DunhillClear1.webp'
+import dunHillClearTwo from './src/assets/DunhillClear2.webp'
+import AMCQOne from './src/assets/AlexanderMcQueen1.webp'
+import AMCQTwo from './src/assets/AlexanderMcQueen2.webp'
+import montBlancOne from './src/assets/MontBlanc1.webp'
+import montBlancTwo from './src/assets/MontBlanc2.webp'
+import gucciOne from './src/assets/Gucci1.webp'
+import gucciTwo from './src/assets/Gucci2.webp'
+import pradaOne from './src/assets/prada1.webp'
+import pradaTwo from './src/assets/prada2.webp'
+import DbOne from './src/assets/Dolcegabbana1.webp'
+import DbTwo from './src/assets/Dolcegabbana2.webp'
+import todsOne from './src/assets/Tods1.webp'
+import todsTwo from './src/assets/Tods2.webp'
+import versaceOne from './src/assets/Versace1.webp'
+import versaceTwo from './src/assets/Versace2.webp'
 
 
 
-export interface product{
+export interface Product{
 
     id:number,
     productTitle:string,
@@ -35,7 +35,7 @@ export interface product{
 
 }
 
-const productList:product[] =[
+const productList: Product[] =[
     
     {
         id:1,
@@ -45,8 +45,8 @@ const productList:product[] =[
         Toningsgrad: Mörka (kategori 3)
         UV-skydd: Ja`,
         productImg: {
-            img1: dunHill1,
-            img2: dunHill2,
+            img1: dunHillOne,
+            img2: dunHillTwo,
         },
         productPrice:4715
     },
@@ -59,8 +59,8 @@ const productList:product[] =[
         UV-skydd: Ja
         Glasögonfodral: Hårt`,
         productImg: {
-            img1:tomFord1,
-            img2:tomFord2,
+            img1:tomFordOne,
+            img2:tomFordTwo,
         },
         productPrice:4299
     },
@@ -73,8 +73,8 @@ const productList:product[] =[
         Toningsgrad: Klara eller väldigt ljusa (kategori 0)
         UV-skydd: Ja`,
         productImg: {
-            img1:dunHillClear1,
-            img2:dunHillClear2,
+            img1:dunHillClearOne,
+            img2:dunHillClearTwo,
         },
         productPrice:4265
     },
@@ -88,8 +88,8 @@ const productList:product[] =[
         UV-skydd: Ja
         Glasögonfodral: Påse med dragsko, hårt`,
         productImg: {
-            img1:AMCQ1,
-            img2:AMCQ2,
+            img1:AMCQOne,
+            img2:AMCQTwo,
         },
         productPrice:3815
     },
@@ -103,8 +103,8 @@ const productList:product[] =[
         UV-skydd: Ja
         Glasögonfodral: Hårt`,
         productImg: {
-            img1:montBlanc1,
-            img2:montBlanc2,
+            img1:montBlancOne,
+            img2:montBlancTwo,
         },
         productPrice:3815
     },
@@ -116,8 +116,8 @@ const productList:product[] =[
         UV-skydd: Ja
         Glasögonfodral: Påse med dragsko, hårt`,
         productImg: {
-            img1:gucci1,
-            img2:gucci2,
+            img1:gucciOne,
+            img2:gucciTwo,
         },
         productPrice:3595
     },
@@ -129,8 +129,8 @@ const productList:product[] =[
         UV-skydd: Ja
         Glasögonfodral: Hårt`,
         productImg: {
-            img1:prada1,
-            img2:prada2,
+            img1:pradaOne,
+            img2:pradaTwo,
         },
         productPrice:3435
     },
@@ -144,8 +144,8 @@ const productList:product[] =[
         UV-skydd: Ja
         Glasögonfodral: Hårt`,
         productImg: {
-            img1:DB1,
-            img2:DB2,
+            img1:DbOne,
+            img2:DbTwo,
         },
         productPrice:3195
     },
@@ -157,8 +157,8 @@ const productList:product[] =[
         Toningsgrad: Medium (kategori 2)
         UV-skydd: Ja`,
         productImg: {
-            img1:tods1,
-            img2:tods2,
+            img1:todsOne,
+            img2:todsTwo,
         },
         productPrice:3145
     },
@@ -170,8 +170,8 @@ const productList:product[] =[
         UV-skydd: Ja
         Glasögonfodral: Hårt`,
         productImg: {
-            img1:versace1,
-            img2:versace2,
+            img1:versaceOne,
+            img2:versaceTwo,
         },
         productPrice:2865
     },
