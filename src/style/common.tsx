712557cca2cb1b7cfcf5855eb12e01645ccd3b3d
David w <cf.davidwong@gmail.com>
2022-06-01 13:07:39 +0200

    Moving folder and updated assets

---
+++ src/style/common.tsx
@@ -0,0 +1,25 @@
+import { CSSProperties } from "react";
+
+export const flexCenter: CSSProperties = {
+
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center'
+
+}
+
+export const flexCenterColumn : CSSProperties = {
+
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    flexDirection:'column'
+}
+
+export const flexCenterRow: CSSProperties ={
+
+    display:'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    flexDirection:'row'
+}
